9628447dba23ce1dcad06cc0f0d37bb2f4eb4c11
Bioconcepts <104314255+Bioconcepts@users.noreply.github.com>
2022-04-24 21:55:53 +0530

    Add files via upload

---
+++ Includes/askus.php
@@ -0,0 +1,45 @@
+
+<section class="text-gray-600 body-font animate__animated animate__fadeInRight">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4">
+      
+      
+      <div class="xl:w-full md:w-full p-4">
+        <div class="p-6 rounded-lg bg-gray-800">
+          <!--<div class="rounded w-full object-cover object-center mb-2" ><center><img src="/Images/activities.png" width="150"/></center></div>-->
+          
+<svg style="opacity:0.2; color:#fff; overflow: hidden; position: absolute;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+</svg>
+          <h2 class="text-xl text-white font-semibold title-font mb-4">Ask us your Queries?</h2>
+          <p class="leading-relaxed text- text-white">Send us your enquiries and our team will be glad to help you out. Get more information about our batch timings.</p>
+          <br/>
+      
+                  <button class="ml-3 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none rounded text-lg"  id="btn">
+                    <font style="vertical-align: inherit;">
+                      <font style="vertical-align: inherit; ">Ask Now</font>
+                    </font>
+                  </button>
+                  
+        </div>
+        
+        
+    <svg style="opacity:0.4; color:#fff; overflow: hidden; position: absolute; margin-left: 40%; margin-top:-60px;" xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+</svg>
+        
+            <svg style="opacity:0.2; color:#fff; overflow: hidden; position: absolute; margin-left: 40%; margin-top:-250px;" xmlns="http://www.w3.org/2000/svg" width="180" height="180" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+</svg>
+
+
+            <svg style="opacity:0.1; color:#fff; overflow: hidden; position: absolute; margin-left: 20%; margin-top:-180px;" xmlns="http://www.w3.org/2000/svg" width="110" height="110" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+</svg>
+        
+        
+      </div>
+      
+    </div>
+  </div>
+</section>
